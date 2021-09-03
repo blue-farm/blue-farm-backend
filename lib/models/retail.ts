8c4ee2c5a0cc9d848ec99e8f75ff1e740272dc0f
mdreamh <mdreamh@gmail.com>
2021-09-03 17:54:36 +0900

    get edit done

--- lib/models/retail.ts
+++ lib/models/retail.ts
@@ -13,12 +13,9 @@ export const findAll = (sort: any, pageIdx: number, callback: Function) => {
 
     const queryGetAmountString = `
       SELECT 
-        SUM(CASE WHEN isShipped = true THEN amount ELSE 0 END) AS shipped_amount
+        SUM(CASE WHEN isShipped = true THEN amount ELSE 0 END) AS shipped_amount,
         SUM(CASE WHEN isShipped = false THEN amount ELSE 0 END) AS unshipped_amount
       FROM retail;`
-    
-    console.log(queryGetItemString);
-    console.log(queryGetAmountString);
 
     db.query(queryGetItemString + queryGetAmountString, (err: any, result: any) => {
         if (err) { callback(err) }
@@ -63,98 +60,6 @@ export const findAll = (sort: any, pageIdx: number, callback: Function) => {
         callback(null, rstData);
     });    
 }
-
-/*
-export const findAll = (callback: Function) => {
-    const queryString = `
-      SELECT 
-       *
-      FROM retail `
-
-    db.query(queryString, (err: any, result: any) => {
-        if (err) { callback(err) }
-
-        const rows = <RowDataPacket[]>result;
-        const orders: Retail[] = [];
-
-        var moment = require('moment');
-
-        rows.forEach(row => {
-            const order: Retail = {
-                id:         row.id,
-                date:       moment(row.date).format("YYYY-MM-DD"),
-                name:       row.name,
-                amount:     row.amount,
-                phone:      row.phone,
-                addr1:      row.addr1,
-                addr2:      row.addr2,
-                zip:        row.zip,
-                isPaid:     row.isPaid,
-                isShipped:  row.isShipped,
-                delivery:   row.delivery,
-            }
-            orders.push(order);
-        });
-        callback(null, orders);
-    });
-}
-
-
-export const getPage = (sort:string, pageIdx:number, callback: Function) => {
-    const queryString = `
-      SELECT 
-       *
-      FROM retail
-      ORDER BY ${sort}
-      LIMIT ?, ? `
-
-    const pageSize = 2;
-    const firstItem = (pageIdx - 1) * pageSize; 
-    
-    db.query(queryString, [firstItem, pageSize], (err: any, result: any) => {
-        if (err) { callback(err) }
-
-        const rows = <RowDataPacket[]>result;
-        const orders: Retail[] = [];
-
-        var moment = require('moment');
-        
-        rows.forEach(row => {
-            const order: Retail = {
-                id:         row.id,
-                date:       moment(row.date).format("YYYY-MM-DD"),
-                name:       row.name,
-                amount:     row.amount,
-                phone:      row.phone,
-                addr1:      row.addr1,
-                addr2:      row.addr2,
-                zip:        row.zip,
-                isPaid:     row.isPaid,
-                isShipped:  row.isShipped,
-                delivery:   row.delivery,
-            }
-            orders.push(order);
-        });
-        callback(null, orders);
-    });    
-}
-
-// Get total amount based on isShipped column
-export const getTotalAmount = (bShipped: boolean, callback: Function) => {
-    const queryString = `
-      SELECT SUM(CASE WHEN isShipped = ${bShipped} THEN amount ELSE 0 END) AS total_amount
-      FROM retail`
-
-    db.query(queryString, (err: any, result: any) => {
-        if (err) { callback(err) }
-
-        const row = (<RowDataPacket[]>result)[0];
-        const amount: number = row.total_amount;
-
-        callback(null, amount);
-    });
-}
-*/
 
 export const create = (order: Retail, callback: Function) => {
     const queryString = "INSERT INTO retail (id, date, name, amount, phone, addr1, addr2, zip, isPaid, isShipped, delivery) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
--- lib/routes/retail.ts
+++ lib/routes/retail.ts
@@ -19,44 +19,6 @@ router.get("/", async (req, res) => {
         res.status(200).json({ "data": orders });
     });
 });
-/*
-// Get Items within the range
-router.get("/page/:sort/:page", async(req, res) => {
-    const sort: string = req.params.sort;
-    const pageIdx: number = Number(req.params.page);
-
-    retailModel.getPage(sort, pageIdx, (err:Error, orders: Retail[]) =>{
-        if(err){
-            return res.status(500).json({ "errorMessage": err.message });
-        }
-
-        res.status(200).json({ "data": orders });
-    });
-});
-
-// Get total amount shipped
-router.get("/shipped/:bShipped", async(req, res) => {
-    const bShipped: boolean = (req.params.bShipped == "true") ? true : false;
-
-    retailModel.getTotalAmount(bShipped, (err:Error, amount: number) =>{
-        if(err){
-            return res.status(500).json({ "errorMessage": err.message });
-        }
-
-        res.status(200).json({ "total-amount": amount });
-    });
-});
-
-// Get All Items
-router.get("/", async (req, res) => {
-    retailModel.findAll((err: Error, orders: Retail[]) => {
-        if (err) {
-            return res.status(500).json({ "errorMessage": err.message });
-        }
-
-        res.status(200).json({ "data": orders });
-    });
-});*/
 
 // Insert New Item
 router.post("/", async(req, res) => {
