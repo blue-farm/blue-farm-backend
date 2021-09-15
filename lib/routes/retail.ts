c38573e0a75e285fd0457d2df321e1e13ab5256c
songi <thddl0617@naver.com>
2021-09-15 14:37:48 +0900

    get에서 오류 나지 않게

--- lib/routes/retail.ts
+++ lib/routes/retail.ts
@@ -8,10 +8,13 @@ module.exports = router;
 
 // Get All Items
 router.get("/", async (req, res) => {
-    const sort: any = req.query.sort;
-    const pageIdx: number = Number(req.query.page);
-
-    retailModel.findAll(sort, pageIdx, (err: Error, orders: Retail[]) => {
+    let sort: any = req.query.sort;
+    let page: number = Number(req.query.page);
+    if (page == undefined || page == null)
+        page = 0;
+    if (sort == undefined || sort == null)
+        sort = 'date';
+    retailModel.findAll(sort, page, (err: Error, orders: Retail[]) => {
         if (err) {
             return res.status(500).json({ "errorMessage": err.message });
         }
@@ -21,50 +24,50 @@ router.get("/", async (req, res) => {
 });
 
 // Insert New Item
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     const newOrder: Retail = req.body;
 
     retailModel.create(newOrder, (err: Error, orderID: number) => {
-        if(err){
+        if (err) {
             return res.status(500).json({ "errorMessage": err.message });
         }
 
-        res.status(200).json({"orderId": orderID});
+        res.status(200).json({ "orderId": orderID });
     });
 });
 
 // Get One Item
-router.get("/:id", async(req, res) => {
+router.get("/:id", async (req, res) => {
     const orderID: number = Number(req.params.id);
 
-    retailModel.findOne(orderID, (err:Error, order:Retail) =>{
-        if(err){
+    retailModel.findOne(orderID, (err: Error, order: Retail) => {
+        if (err) {
             return res.status(500).json({ "errorMessage": err.message });
         }
 
-        res.status(200).json({"data": order});
+        res.status(200).json({ "data": order });
     });
 });
 
 // Edit One Item
-router.put("/:id", async(req, res) => {
+router.put("/:id", async (req, res) => {
     const order: Retail = req.body;
 
     retailModel.update(order, (err: Error) => {
-        if(err){
+        if (err) {
             return res.status(500).json({ "errorMessage": err.message });
         }
-  
+
         res.status(200).send();
     })
 });
 
 // Delete One Item
-router.delete("/:id", async(req, res) => {
+router.delete("/:id", async (req, res) => {
     const orderID: number = Number(req.params.id);
 
-    retailModel.deleteOne(orderID, (err:Error) =>{
-        if(err){
+    retailModel.deleteOne(orderID, (err: Error) => {
+        if (err) {
             return res.status(500).json({ "errorMessage": err.message });
         }
 
