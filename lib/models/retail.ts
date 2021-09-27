0de16606b00aee19e9c12306c3d09f1de025d396
songi <thddl0617@naver.com>
2021-09-27 17:52:17 +0900

    test

--- lib/models/retail.ts
+++ lib/models/retail.ts
@@ -11,7 +11,7 @@ export const findAll = (sort: any, pageIdx: number, isShipped: boolean, callback
        *
       FROM retail
       ORDER BY ${sort}`
-    if (isShipped != undefined || isShipped != null || isShipped != NaN)
+    if (isShipped != undefined && isShipped != null )
         queryGetItemString += ` WHERE isShipped=${isShipped}`
 
     queryGetItemString += + ` LIMIT ${pageIdx * 20}, 20;`
